588404b842ee8296caf93ae10fd903282a876ab7
Meng Yuan <13714858@student.uts.edu.au>
2022-09-11 18:57:32 +0930

    create layout file structure

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,50 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
-import create from 'zustand'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+import create from "zustand";
+
+import NavBar from "./components/NavBar";
+import SideMenu from "./components/SideMenu";
+import Content from "./components/Content";
 
 const useBearStore = create((set) => ({
   bears: 0,
   increasePopulation: () => set((state) => ({ bears: state.bears + 1 })),
   decreasePopulation: () => set((state) => ({ bears: state.bears - 1 })),
   removeAllBears: () => set({ bears: 0 }),
-}))
+}));
 
 function App() {
-
-  const bears = useBearStore((state) => state.bears)
-  const increasePopulation = useBearStore((state) => state.increasePopulation)
-  const decreasePopulation = useBearStore((state) => state.decreasePopulation)
-  const removeAllBears = useBearStore((state) => state.removeAllBears)
-
+  const bears = useBearStore((state) => state.bears);
+  const increasePopulation = useBearStore((state) => state.increasePopulation);
+  const decreasePopulation = useBearStore((state) => state.decreasePopulation);
+  const removeAllBears = useBearStore((state) => state.removeAllBears);
 
   return (
     <div className="App">
-      <div>
-        
-      </div>
-      <h1>🐻BEAR!</h1>
-      <div className="card">
-        <h1>{bears} around here ...</h1>
-        <button onClick={increasePopulation}>
-          Bear up
-        </button>
-        <button onClick={decreasePopulation}>
-          Bear down
-        </button>
-        <button onClick={removeAllBears}>
-          Bear gone
-        </button>
-
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <NavBar />
+      <SideMenu />
+      <Content />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -3,20 +3,20 @@ import { Box, Flex, Text } from "@chakra-ui/react";
 
 const NavBar = () => {
   return (
-    <Box bg="BG" shadow="md">
-      <Box
-        m="auto"
-        w="100%"
-        maxW={1440}
-        diplay="flex"
-        justifyContent="space-between"
-        px="1rem"
-        py="0.5rem"
-      >
-        <Text fontSize="2xl" fontWeight="bold" width="auto">
-          Capstone project
-        </Text>
-      </Box>
+    <Box
+      m="auto"
+      w="100%"
+      diplay="flex"
+      justifyContent="space-between"
+      px="1rem"
+      py="0.5rem"
+      border="1px"
+      borderColor="#ddd"
+      bg="BG"
+    >
+      <Text fontSize="2xl" fontWeight="bold" width="auto">
+        Capstone project
+      </Text>
     </Box>
   );
 };
