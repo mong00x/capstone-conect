82f4c209c4fb6a7b4d9f574344e805f08d728e29
Meng Yuan <13714858@student.uts.edu.au>
2022-09-16 23:47:30 +0930

    consume project state from store in MainContent

--- client/src/components/MainContent/MainContent.jsx
+++ client/src/components/MainContent/MainContent.jsx
@@ -3,24 +3,25 @@ import ProjectCard from "./ProjectCard";
 import { Box, Flex, Grid } from "@chakra-ui/react";
 import SearchFilter from "./SearchFilter";
 
+import useStore from "../../store";
+
 const MainContent = () => {
   // register project to store
+  const projects = useStore((state) => state.projects);
+  console.log(projects);
   return (
     <Flex flexDir="column" w="100%" h="100%" color="DarkShades" bg="ContentBG">
       <SearchFilter />
       <Grid
-        templateColumns="repeat(auto-fill, minmax(300px, 1fr))"
+        templateColumns="repeat(auto-fill, minmax(360px, 1fr))"
         gap={8}
         p="32px 32px 120px 32px"
         justifyContent="space-between"
         overflow={"auto"}
       >
-        <ProjectCard id={1} />
-        <ProjectCard id={2} />
-        <ProjectCard id={3} />
-        <ProjectCard id={4} />
-        <ProjectCard id={5} />
-        <ProjectCard id={6} />
+        {projects.map((project) => (
+          <ProjectCard key={project.id} project={project} />
+        ))}
       </Grid>
     </Flex>
   );
