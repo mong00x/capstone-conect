e1a9469d61b605efbe8097bffe8e2218424a448f
Meng Yuan <13714858@student.uts.edu.au>
2022-09-16 12:00:05 +0930

    fix typo

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -9,7 +9,7 @@ const theme = extendTheme({
     ContentBG: "#F5F5F5",
     LightShades: "#F2F2F2",
     DarkShades: "#363A66",
-    AccentLight: "9BADCD",
+    AccentLight: "#9BADCD",
     AccentMain: {
       light: "#E9D8FF",
       default: "#9747FF",
