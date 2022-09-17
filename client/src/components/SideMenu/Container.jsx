ee64855d87675f12fc3556ecf963d9a86f14a767
Meng Yuan <13714858@student.uts.edu.au>
2022-09-18 00:16:11 +0930

    consume Rank from store and pass down to card components

--- client/src/components/SideMenu/Container.jsx
+++ client/src/components/SideMenu/Container.jsx
@@ -3,17 +3,30 @@ import { memo, useCallback, useState } from "react";
 import { useDrop } from "react-dnd";
 import { Card } from "./Cards";
 import { ItemTypes } from "./ItemTypes";
+
+import useStore from "../../store";
+
 const style = {
   width: "100%",
 };
 
 //  get item data from store
-const ITEMS = [];
+
 const Container = memo(function Container() {
-  const [cards, setCards] = useState(ITEMS);
+  const Rank = useStore((state) => state.Rank);
+  console.log("Rank", Rank);
+  const [cards, setCards] = useState(Rank);
+  useStore.subscribe(
+    (state) => {
+      setCards(state.Rank);
+    },
+    (state) => state.Rank
+  );
+
   const findCard = useCallback(
+    // useCallback is a hook that returns a memoized version of the callback that only changes if one of the dependencies has changed. the function will only be called if one of the dependencies has changed.
     (id) => {
-      const card = cards.filter((c) => `${c.id}` === id)[0];
+      const card = cards.filter((c) => `${c.id}` === id)[0]; // filter returns an array of all elements that pass the test implemented by the provided function. [0] returns the first element of the array.
       return {
         card,
         index: cards.indexOf(card),
@@ -36,20 +49,19 @@ const Container = memo(function Container() {
     [findCard, cards, setCards]
   );
   const [, drop] = useDrop(() => ({ accept: ItemTypes.CARD }));
-  return ITEMS == [] ? (
+  return (
     <div ref={drop} style={style}>
       {cards.map((card) => (
         <Card
           key={card.id}
-          id={`${card.id}`}
-          text={card.text}
+          id={card.id}
+          text={card.topic}
+          supervisors={card.supervisors}
           moveCard={moveCard}
           findCard={findCard}
         />
       ))}
     </div>
-  ) : (
-    <div style={style}>Please select up to 3 projects</div>
   );
 });
 
