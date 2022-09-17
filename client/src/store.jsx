faf6b95f38b47d3e96767f330685610ad200ce62
Meng Yuan <13714858@student.uts.edu.au>
2022-09-18 00:13:03 +0930

    add project number to data & fix controlers

--- client/src/store.jsx
+++ client/src/store.jsx
@@ -4,6 +4,7 @@ const useStore = create((set) => ({
   projects: [
     {
       id: 1,
+      number: 1,
       topic: "Machine learning approaches for Cyber Security",
       description:
         "As we use internet more, the data produced by us is enormous. But are these data being secure? The goal of applying machine learning or intelligence is to better risk modelling and prediction and for an informed decision support. Students will be working with either supervised or unsupervised machine learning approaches to solve the problem/s in the broader areas of Cyber Security.The Transportation Security Administration (TSA) has a complex security program that requires officers to visually search travelers’ baggage for prohibited objects, or cues to such objects, which may pose a threat to security. This visual search - when conducted by humans - is difficult to successfully complete as it may result in either a false positive (finding a non-threatening object) or a false negative (missing a threatening object); indeed, a recent assessment reported that TSA screeners missed 95% percent of mock explosive and banned weapons smuggled through checkpoints (ABC News, 2015). It is possible that effective screening may be achieved via the coupling of human screeners and detection technology. Here, the proposal is to design an algorithm that analyses x-ray images during baggage (and/or person) screening to automatically give an alert when a potential threat is detected. The challenge is to utilise deep neural networks to aid with image and object recognition; specifically, to detect the presence of a threat within compact, cluttered and diverse baggage content in a timely-manner, thus providing travel safety assurances.",
@@ -18,10 +19,11 @@ const useStore = create((set) => ({
     },
     {
       id: 2,
+      number: 2,
       topic:
         "Enhancing airport screening using a deep neural network algorithm",
       description:
-        "The Transportation Security Administration (TSA) has a complex security program that requires officers to visually search travelers’ baggage for prohibited objects, or cues to such objects, which may pose a threat to security. This visual search - when conducted by humans - is difficult to successfully complete as it may result in either a false positive (finding a non-threatening object) or a false negative (missing a threatening object); indeed, a recent assessment reported that TSA screeners missed 95% percent of mock explosive and banned weapons smuggled through checkpoints (ABC News, 2015). It is possible that effective screening may be achieved via the coupling of human screeners and detection technology. Here, the proposal is to design an algorithm that analyses x-ray images during baggage (and/or person) screening to automatically give an alert when a potential threat is detected. The challenge is to utilise deep neural networks to aid with image and object recognition; specifically, to detect the presence of a threat within compact, cluttered and diverse baggage content in a timely-manner, thus providing travel safety assurances.\rPotential issues that require solutions:\r1.	Utilise a machine learning framework – based on convolutional neural networks, for example - that processes images captured either by checkpoint baggage screeners or body scanners then detects threatening objects. These objects are usually intentionally obscured by other items, thus a computer algorithm that automatically identifies threat is useful in reducing the risk of misdetection during the physical inspection of the x-ray image. The detection must be rapid and accurate, potentially with a better performance rate than that of human screeners, in order to reduce the amount of work done by a human inspector.\r2.	Develop an algorithm that identifies regions within the images captured either by checkpoint baggage screeners or body scanners that may include threatening objects. This alternative solution enables the human screener to then review only the alerted regions of the image, thus not only reducing the amount of time they need to scan the entire image for a potential threat, but also enabling them to focus all cognitive effort on alerted regions.\r3.	Minimise the false alarm rate when detecting non-threatening objects in images captured either by checkpoint baggage screeners or body scanners. This point is pertinent as it would reduce the need for checkpoint workers to pull passengers aside for further inspection.",
+        "The Transportation Security Administration (TSA) has a complex security program that requires officers to visually search travelers’ baggage for prohibited objects, or cues to such objects, which may pose a threat to security. This visual search - when conducted by humans - is difficult to successfully complete as it may result in either a false positive (finding a non-threatening object) or a false negative (missing a threatening object); indeed, a recent assessment reported that TSA screeners missed 95% percent of mock explosive and banned weapons smuggled through checkpoints (ABC News, 2015). It is possible that effective screening may be achieved via the coupling of human screeners and detection technology. Here, the proposal is to design an algorithm that analyses x-ray images during baggage (and/or person) screening to automatically give an alert when a potential threat is detected. The challenge is to utilise deep neural networks to aid with image and object recognition; specifically, to detect the presence of a threat within compact, cluttered and diverse baggage content in a timely-manner, thus providing travel safety assurances.\rPotential issues that require solutions:\r1. Utilise a machine learning framework – based on convolutional neural networks, for example - that processes images captured either by checkpoint baggage screeners or body scanners then detects threatening objects. These objects are usually intentionally obscured by other items, thus a computer algorithm that automatically identifies threat is useful in reducing the risk of misdetection during the physical inspection of the x-ray image. The detection must be rapid and accurate, potentially with a better performance rate than that of human screeners, in order to reduce the amount of work done by a human inspector.\r2. Develop an algorithm that identifies regions within the images captured either by checkpoint baggage screeners or body scanners that may include threatening objects. This alternative solution enables the human screener to then review only the alerted regions of the image, thus not only reducing the amount of time they need to scan the entire image for a potential threat, but also enabling them to focus all cognitive effort on alerted regions.\r3. Minimise the false alarm rate when detecting non-threatening objects in images captured either by checkpoint baggage screeners or body scanners. This point is pertinent as it would reduce the need for checkpoint workers to pull passengers aside for further inspection.",
       keywords: [
         "Internal",
         "External",
@@ -34,9 +36,10 @@ const useStore = create((set) => ({
     },
     {
       id: 3,
+      number: 4,
       topic: "Augmented Reality System on Improving Safety of Young Drivers",
       description:
-        "Young drivers (L- and P-platers) are over-represented when examining car-related deaths in Australia. This over-representation may stem from young drivers’ lack of experience with searching dynamic and complex visual environments in order to detect task-relevant items, including traffic signs, pedestrians and other cars, or gauging distances between their own car and others (i.e., depth perception). Augmented-reality may serve as an effective tool that enhances the driving performance of young drivers by graphically overlaying visual information - that young drivers may miss or find difficulty calculating in their environment - onto the real world. The challenge here is, therefore, to design an augmented-reality interface that can increase the situational awareness (ability to recognise task-relevant items), and, thus, improve the decision- making (safe driving) of young drivers without increasing their cognitive or perceptual workload.\rPotential issues that require solutions:\r1.	Identify the difficulties that young drivers may face whilst driving in Australia.\r2.	Develop an augmented-reality head-up display that would help resolve the difficulties.\r3.	Improve the usability of the augmented-reality platform by making it multi-sensory (visual and auditory display).",
+        "Young drivers (L- and P-platers) are over-represented when examining car-related deaths in Australia. This over-representation may stem from young drivers’ lack of experience with searching dynamic and complex visual environments in order to detect task-relevant items, including traffic signs, pedestrians and other cars, or gauging distances between their own car and others (i.e., depth perception). Augmented-reality may serve as an effective tool that enhances the driving performance of young drivers by graphically overlaying visual information - that young drivers may miss or find difficulty calculating in their environment - onto the real world. The challenge here is, therefore, to design an augmented-reality interface that can increase the situational awareness (ability to recognise task-relevant items), and, thus, improve the decision- making (safe driving) of young drivers without increasing their cognitive or perceptual workload.\rPotential issues that require solutions:\r1. Identify the difficulties that young drivers may face whilst driving in Australia.\r2. Develop an augmented-reality head-up display that would help resolve the difficulties.\r3.Improve the usability of the augmented-reality platform by making it multi-sensory (visual and auditory display).",
       keywords: [
         "Internal",
         "External",
@@ -49,6 +52,7 @@ const useStore = create((set) => ({
     },
     {
       id: 4,
+      number: 8,
       topic:
         "A data science approach in analysing a complex social-environmental system (CS, SE, IS)",
       description:
@@ -64,6 +68,7 @@ const useStore = create((set) => ({
     },
     {
       id: 5,
+      number: 9,
       topic: "Informetrics applications in multidisciplinary domain",
       description:
         "Informetrics studies are concerned with the quantitative aspects of information. The applications of advanced machine learning, information retrieval, network science and bibliometric techniques on various information artefact have contributed fresh insights into the evolutionary nature of research fields. This project aims at discovering informetric properties of multidisciplinary research literature using various machine learning, network analysis, data visualisation and data wrangling tools.",
@@ -86,7 +91,17 @@ const useStore = create((set) => ({
       supervisors: ["Dr. Charles Yeo ", "Dr. Bharanidharan Shanmugam"],
     },
   ],
-  Rank: [], // store the rank of each project selected, first project selected will be rank one, second project selected will be rank two, etc.
+  Rank: [],
+  // addRank function will add selcted project topic and supervisor to Rank array
+  addRank: (id, topic, supervisors) =>
+    set((state) => ({
+      Rank: [
+        ...state.Rank,
+        { id, topic, supervisors, rank: state.Rank.length + 1 },
+      ],
+    })),
+  removeRank: (id) =>
+    set((state) => ({ Rank: state.Rank.filter((i) => i.id !== id) })),
 }));
 
 export default useStore;
