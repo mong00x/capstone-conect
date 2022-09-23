6e3b4c21b1a16e0aec69e976e642f9fdf9b76d0f
Meng Yuan <s342742@students.cdu.edu.au>
2022-09-24 01:08:21 +0930

    add spinnier during loading

--- client/src/components/MainContent/MainContent.jsx
+++ client/src/components/MainContent/MainContent.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ProjectCard from "./ProjectCard";
-import { Flex, Grid } from "@chakra-ui/react";
+import { Flex, Grid, Spinner } from "@chakra-ui/react";
 import SearchFilter from "./SearchFilter";
 
 import axios from "axios";
@@ -29,24 +29,38 @@ const MainContent = () => {
       height="100vh"
       width="100vw"
     >
-      <h1>Loading...</h1>
+      <Spinner
+        thickness="4px"
+        speed="0.65s"
+        emptyColor="gray.200"
+        color="blue.500"
+        size="xl"
+      />
     </Flex>
   ) : (
-    <Flex flexDir="column" w="100%" h="100%" color="DarkShades" bg="ContentBG">
-      <SearchFilter />
-      <Grid
-        templateColumns="repeat(auto-fill, minmax(360px, 1fr))"
-        gap={8}
-        p="32px 32px 120px 32px"
-        justifyContent="space-between"
-        overflow={"auto"}
+    isSuccess && (
+      <Flex
+        flexDir="column"
+        w="100%"
+        h="100%"
+        color="DarkShades"
+        bg="ContentBG"
       >
-        {data &&
-          data.map((project) => (
-            <ProjectCard key={project.project_id} project={project} />
-          ))}
-      </Grid>
-    </Flex>
+        <SearchFilter />
+        <Grid
+          templateColumns="repeat(auto-fill, minmax(360px, 1fr))"
+          gap={8}
+          p="32px 32px 120px 32px"
+          justifyContent="space-between"
+          overflow={"auto"}
+        >
+          {data &&
+            data.map((project) => (
+              <ProjectCard key={project.project_id} project={project} />
+            ))}
+        </Grid>
+      </Flex>
+    )
   );
 };
 
