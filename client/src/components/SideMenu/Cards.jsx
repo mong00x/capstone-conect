e82a82fc4ed4b2ed8758ce7dcb218f78c8e875cf
Meng Yuan <s342742@students.cdu.edu.au>
2022-09-30 23:44:57 +0930

    rankcard animation

--- client/src/components/SideMenu/Cards.jsx
+++ client/src/components/SideMenu/Cards.jsx
@@ -1,9 +1,9 @@
-import { memo } from "react";
+import { memo, useEffect, useRef } from "react";
 import { useDrag, useDrop } from "react-dnd";
 import { ItemTypes } from "./ItemTypes";
 import PropTypes from "prop-types";
 
-import { Flex, Text, Box } from "@chakra-ui/react";
+import { Flex, Text, Box, Fade } from "@chakra-ui/react";
 import { DragHandleIcon } from "@chakra-ui/icons";
 
 export const Card = memo(function Card({
@@ -13,6 +13,16 @@ export const Card = memo(function Card({
   moveCard,
   findCard,
 }) {
+  const existRef = useRef(true);
+  useEffect(() => {
+    existRef.current = true;
+    console.log("mounting", existRef.current);
+    return () => {
+      existRef.current = false;
+      console.log("unmounting", existRef.current);
+    };
+  }, []);
+
   const originalIndex = findCard(id).index;
   const [{ isDragging }, drag] = useDrag(
     () => ({
@@ -45,33 +55,37 @@ export const Card = memo(function Card({
   );
   const opacity = isDragging ? 0.5 : 1;
   return (
-    <Flex
-      flexDir="row"
-      ref={(node) => drag(drop(node))}
-      opacity={opacity}
-      alignItems="center"
-      borderRadius="md"
-      my={2}
-      p={2}
-      gap={4}
-      bg="gray.200"
-      cursor="move"
-    >
-      <Box>
-        <DragHandleIcon />
-      </Box>
-      <Flex flexDir="column" gap={2}>
-        <Text fontWeight="bold" lineHeight="20px">
-          {text && text}
-        </Text>
+    <Fade in={existRef.current}>
+      <Flex
+        className="RankCard"
+        flexDir="row"
+        ref={(node) => drag(drop(node))}
+        opacity={opacity}
+        alignItems="center"
+        borderRadius="md"
+        my={2}
+        p={2}
+        gap={4}
+        bg="gray.200"
+        cursor="move"
+        h="128px"
+      >
         <Box>
-          {supervisors &&
-            supervisors.map((supervisor) => (
-              <Text fontSize="sm">{supervisor}</Text>
-            ))}
+          <DragHandleIcon />
         </Box>
+        <Flex flexDir="column" gap={2}>
+          <Text fontWeight="bold" lineHeight="20px">
+            {text && text}
+          </Text>
+          <Box>
+            {supervisors &&
+              supervisors.map((supervisor) => (
+                <Text fontSize="sm">{supervisor}</Text>
+              ))}
+          </Box>
+        </Flex>
       </Flex>
-    </Flex>
+    </Fade>
   );
 });
 
