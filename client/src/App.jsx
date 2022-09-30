e78132988d52ded6b4a72cac4add685ae5026747
Meng Yuan <s342742@students.cdu.edu.au>
2022-09-30 13:17:58 +0930

    delete unused code in App

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,32 +1,15 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
 import "./App.css";
 import create from "zustand";
-
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-
 import { Box, Flex, Text } from "@chakra-ui/react";
-
 import NavBar from "./components/NavBar";
 import SideMenu from "./components/SideMenu/SideMenu";
 import MainContent from "./components/MainContent/MainContent";
 
-const useBearStore = create((set) => ({
-  bears: 0,
-  increasePopulation: () => set((state) => ({ bears: state.bears + 1 })),
-  decreasePopulation: () => set((state) => ({ bears: state.bears - 1 })),
-  removeAllBears: () => set({ bears: 0 }),
-}));
-
 const queryClient = new QueryClient();
 
 function App() {
-  const bears = useBearStore((state) => state.bears);
-  const increasePopulation = useBearStore((state) => state.increasePopulation);
-  const decreasePopulation = useBearStore((state) => state.decreasePopulation);
-  const removeAllBears = useBearStore((state) => state.removeAllBears);
-
   return (
     <Flex
       className="App"
