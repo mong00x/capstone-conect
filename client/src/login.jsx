cc3275166decc9366c0ff37e645288387cd57cf0
Abinash niure <82077593+Abinash083@users.noreply.github.com>
2022-10-11 14:53:42 +0930

    Update login.jsx
    sesson set up

--- client/src/login.jsx
+++ client/src/login.jsx
@@ -6,10 +6,13 @@ import Modal from "./popup";
 
 const Login_page = () => {
     const [email,setEmail] = useState('');
+    const [fname,setFname] = useState('');
     const [user_type,setType] = useState('student');
     const [password,setPassword] = useState(Math.floor(Math.random() * (9999 - 1111)) + 1111);
-    
     const [popup,setPopup] = useState(false);
+    
+    const user = {name: fname, email: email, password_token: password, auth: false}
+    sessionStorage.setItem('user', JSON.stringify(user))
 
     const handellogin = (e) =>{
         e.preventDefault();
@@ -24,8 +27,13 @@ const Login_page = () => {
             }, function(error) {
                console.log('FAILED...', error);
             });
+        
+        const user = {name: fname, email: email, password_token: password, auth: false}
+        sessionStorage.setItem('user', JSON.stringify(user))
+        console.log(JSON.parse(sessionStorage.getItem('user')))
 
       setPopup(true)
+
         
     }
 
@@ -50,10 +58,18 @@ const Login_page = () => {
             name="email"
             value={email}
             onChange={(e) => setEmail(e.target.value)}/>}
-            
-           
-            
             <br></br>
+
+            {user_type === "student" && <label  >Full name : </label>}
+            <br></br>
+            {user_type === "student" && <input 
+            type="text"
+            id="fname"
+            name="fname"
+            value={fname}
+            onChange={(e) => setFname(e.target.value)}/>}
+            <br></br>
+
             {user_type === "student" && <button  type="submit">Send Password</button>}
             {user_type !== "student" && <a href="http://localhost/login.php"  >Login as {user_type}</a>}
         </form>
@@ -63,7 +79,7 @@ const Login_page = () => {
     return(
         
         <div >
-        <Modal pin={password} email={email}/>
+        <Modal pin={password} email={email} fname= {fname}/>
         </div>
         
     )
