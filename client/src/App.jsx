f4d185a9adf3978bfc1a244f3d8116e5e429fd8d
Meng Yuan <s342742@students.cdu.edu.au>
2022-10-11 21:31:03 +0930

    removed unused import

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,7 @@
 import "./App.css";
-import create from "zustand";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { Box, Flex, Text } from "@chakra-ui/react";
+import {  Flex } from "@chakra-ui/react";
 import NavBar from "./components/NavBar";
 import SideMenu from "./components/SideMenu/SideMenu";
 import MainContent from "./components/MainContent/MainContent";
