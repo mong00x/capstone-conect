a2815344e75cd271d843e9425ca5909343551753
Abinash <avinashniure026@gmail.com>
2022-10-13 14:50:16 +0930

    clean up code

--- client/src/popup.jsx
+++ client/src/popup.jsx
@@ -1,33 +1,28 @@
 import react from 'react';
 import {useState} from "react";
-import axios from "axios";
 
-const Modal = ( {pin,email,fname,studentid}) => 
+const Modal = () => 
 {
     const [typedpin,setpin] = useState("");
     
     function Verify()
-{
-    if (pin==typedpin)
     {
-        const user = {studentid: studentid, name: fname, email: email, password_token: pin, auth: true}
-        sessionStorage.setItem('user', JSON.stringify(user))
-        
-        
-        location.replace ("http://localhost/add_student.php?x=" + sessionStorage.getItem('user')) 
-}
+        if (JSON.parse(sessionStorage.getItem('user')).pin==typedpin)
+        {
+            location.replace ("http://localhost/add_student.php?x=" + sessionStorage.getItem('user')) 
+        }
 
-    else{
-        document.getElementById("error").innerHTML = "Wrong pin try again!";
-    }
+        else{
+            document.getElementById("error").innerHTML = "Wrong pin try again!";
+        }
     
-}
+    }
     return(
         <div className='popup'>
             
             <div className='popinside'>
                 <p id='error'> </p>
-                varification code has been send to {email}.
+                varification code has been send to { JSON.parse(sessionStorage.getItem('user')).email }.
                 <br></br>
                 <br></br>
                 
@@ -45,8 +40,7 @@ const Modal = ( {pin,email,fname,studentid}) =>
 
             </div>
 
-        </div>
-    )
+        </div>)
 }
 
 
