a192d82d2aeb65d739ee4479fa6c94de536b7391
Abinash <avinashniure026@gmail.com>
2022-10-17 18:57:11 +0930

    mail sending to lecturer

--- server/add_project_register.php
+++ server/add_project_register.php
@@ -89,6 +89,7 @@ if (!empty($data['name']) && !empty($data['email']) && !empty($data['studentid']
 
         
         $project_topic = $row['project_topic'];
+        $lecturer_id = $row['lecturer_id'];
         
         // get student information
         $query = "SELECT student_name, student_id FROM students WHERE student_id = '$student_id'";
@@ -101,6 +102,17 @@ if (!empty($data['name']) && !empty($data['email']) && !empty($data['studentid']
         
         $student_id = $row['student_id'];
 
+
+        //get lecturer email
+        $query = "SELECT lecturer_email FROM lecturers WHERE lecturer_id = '$lecturer_id'";
+        connectDB();
+        $result=mysqli_query($_SESSION['db'],$query) or die ("<b>A fatal MySQL error occured</b>.\n<br />Query: " . $query . "<br />\nError: (" . mysqli_errno($_SESSION['db']) . ") " . mysqli_error($_SESSION['db']) . "Data: " . $data);
+        closeDB();
+        $row = mysqli_fetch_assoc($result);
+       
+
+        $lecturer_email = $row['lecturer_email'];
+
         $message = file_get_contents("lecturer_email_template.html");
         $message = str_replace("%project_topic%", $project_topic, $message);
         $message = str_replace("%student_name%", $student_name, $message);
@@ -108,12 +120,12 @@ if (!empty($data['name']) && !empty($data['email']) && !empty($data['studentid']
 
         
 
-        $LECmail->Subject = 'Application received';
+        $LECmail->Subject = 'New project request';
         $LECmail->isHTML(true);
         $LECmail->msgHTML($message);
-        $LECmail->AltBody = 'This is a plain-text message body';
+        $LECmail->AltBody = 'You have a new project';
         $LECmail->setFrom('admin@udlcanada.com'); // sender
-        $LECmail->addAddress('s342742@students.cdu.edu.au'); // receiver
+        $LECmail->addAddress('$lecturer_email '); // receiver
         if ($LECmail->Send()) {
             echo " Lecturer Mail sent";
         } else {
