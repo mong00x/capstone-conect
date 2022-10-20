1d12383d4ed274df2b448fd80eca3cd48bfb7bee
Abinash <avinashniure026@gmail.com>
2022-10-20 16:32:40 +0930

    checking if student id already exist

--- client/src/popup.jsx
+++ client/src/popup.jsx
@@ -1,6 +1,6 @@
 import react from 'react';
 import {useState} from "react";
-import axios from "axios";
+import md5 from "md5";
 
 const Modal = ( {pin,email,fname,studentid}) => 
 
@@ -9,21 +9,19 @@ const Modal = ( {pin,email,fname,studentid}) =>
     const [typedpin,setpin] = useState("");
     const redirectUrl = process.env.NODE_ENV === "development" ? "http://localhost/add_student.php?x=" : "https://cduprojects.spinetail.cdu.edu.au/adminpage/add_student.php?x=";    
     function Verify()
-{
-    if (pin==typedpin)
     {
-        const user = {studentid: studentid, name: fname, email: email, password_token: pin, auth: true}
-        sessionStorage.setItem('user', JSON.stringify(user))
-        
-        
-        location.replace (redirectUrl + sessionStorage.getItem('user')) 
-}
+        if (pin==typedpin)
+        {
+            const user = {studentid: studentid, name: fname, email: email, password_token: pin, auth: true}
+            sessionStorage.setItem('user', JSON.stringify(user))
+            location.replace (redirectUrl + sessionStorage.getItem('user')) 
+        }
 
-    else{
-        document.getElementById("error").innerHTML = "Wrong pin try again!";
+        else{
+            document.getElementById("error").innerHTML = "Wrong pin try again!";
+        }
+        
     }
-    
-}
     return(
         <div className='popup'>
             
--- server/add_student.php
+++ server/add_student.php
@@ -10,6 +10,22 @@ $fname= $jo->name;
 $email= $jo->email;
 $id= $jo->studentid;
 
+$query = "SELECT * FROM students WHERE student_id='$id'";
+connectDB();
+
+$result=mysqli_query($_SESSION['db'],$query) or die ("<b>A fatal MySQL error occured</b>.\n<br />Query: " . $query . "<br />\nError: (" . mysqli_errno($_SESSION['db']) . ") " . mysqli_error($_SESSION['db']));
+
+closeDB();
+	if(mysqli_num_rows($result)>=1) {
+	 echo "Student with this student id have already exist";?>
+	<script>
+		alert("This studentId have already been registered ")
+		consol.log("student with this id already exist")
+		window.location.href = "http://127.0.0.1:5173/";
+	</script>
+	<?php
+} else {
+
 $query= "INSERT INTO students (student_id, student_name, student_email, student_password_token) VALUES ('$id','$fname', '$email', '$password_token')";
 connectDB();
 	$result=mysqli_query($_SESSION['db'],$query) or die ("<b>A fatal MySQL error occured</b>.\n<br />Query: " . $query . "<br />\nError: (" . mysqli_errno($_SESSION['db']) . ") " . mysqli_error($_SESSION['db']));
@@ -18,15 +34,11 @@ closeDB();
 		$success="1";
 		$_SESSION['msg']="User Created";	
 		$_SESSION['msgType']="success";
+}
 ?>
 <script type="text/javascript">
 	// environment variables if development 
 	location.replace("http://localhost:5173/app")
-
-
-
-
-
 
 	// environment variables if production
 	// location.replace("https://cduprojects.spinetail.cdu.edu.au/app")
