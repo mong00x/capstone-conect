b7ace516f26542bf69590849739196488ac0f00f
Khanhdzudo <89830152+Khanhdzudo@users.noreply.github.com>
2022-10-23 20:39:40 +0930

    Add files via upload

--- server/decline.php
+++ server/decline.php
@@ -5,7 +5,7 @@ require("includes.php");
 if (isset($_GET['decline']))
 {
 	
-	$query="UPDATE student_project_requests SET approve=2 WHERE project_id=".$_GET['decline']." AND student_id=".$_GET['student_id'];
+	$query="UPDATE student_project_requests SET approve=2 WHERE project_id=".$_GET['project_id']." AND student_id=".$_GET['student_id'];
 	connectDB();
 		$result = mysqli_query($_SESSION['db'],$query) or die("<p><b>A fatal MySQL error occured</b>.\n<br />Query: " . $query . "<br />\nError: (" . mysqli_errno($_SESSION['db']) . ") " . mysqli_error($_SESSION['db']) . "</p>");
 		closeDB();
