b06cf84087e4a5e533b1129c943755225ad5c08f
Meng Yuan <s342742@students.cdu.edu.au>
2022-11-02 00:49:53 +0930

    conndition decline in case already approved/declined

--- server/decline.php
+++ server/decline.php
@@ -2,14 +2,26 @@
 require("includes.php");
 
 
-if (isset($_GET['decline']))
+if (isset($_GET['decline'])) // 
 {
-	
-	$query="UPDATE student_project_requests SET approve=2 WHERE project_id=".$_GET['project_id']." AND student_id=".$_GET['student_id'];
+	$query = "SELECT *  FROM student_project_requests WHERE project_id=".$_GET['project_id']." AND student_id=".$_GET['student_id'];
 	connectDB();
+	$result = mysqli_query($_SESSION['db'], $query);
+	closeDB();
+	$approve_result = mysqli_fetch_assoc($result);
+	if ($approve_result["approve"] == 0 ) {
+		$query="UPDATE student_project_requests SET approve=2 WHERE project_id=".$_GET['project_id']." AND student_id=".$_GET['student_id'];
+		connectDB();
 		$result = mysqli_query($_SESSION['db'],$query) or die("<p><b>A fatal MySQL error occured</b>.\n<br />Query: " . $query . "<br />\nError: (" . mysqli_errno($_SESSION['db']) . ") " . mysqli_error($_SESSION['db']) . "</p>");
 		closeDB();
-	
+		$decline="Declined successful, you can now close this window.";
+	}
+	else {
+		$is_declined = ($approve_result["approve"] == 1) ? "Approved" : "Declined";
+		$decline="Failed to decline, the application has already been processed.\n". "Current status: " . $is_declined ."\n" . "Last updated: " . $approve_result["state_changed_time"];
+	}
+
+
 }
 
 ?>
@@ -23,6 +35,11 @@ if (isset($_GET['decline']))
 </head>
 
 <body>
-	<h1>The student register has been declined !</h1>
+	<h1>
+		<?php
+			echo $decline 
+		?>
+	</h1>
+	
 </body>
 </html>
