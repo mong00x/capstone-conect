4ca401cb23e542961f2daf68ccecbbe0b571f784
Meng Yuan <68982813+mong00x@users.noreply.github.com>
2023-07-13 16:21:52 +0930

    save cridentials to .env file

--- client/src/api/supabaseClient.jsx
+++ client/src/api/supabaseClient.jsx
@@ -1,5 +1,9 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabase = createClient("https://ulisxilkyesydfgesodq.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVsaXN4aWxreWVzeWRmZ2Vzb2RxIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY4ODc3NDMzMSwiZXhwIjoyMDA0MzUwMzMxfQ.dyf-HMkj49EMRG3j0TL5gjulqm17ST0YsUJPVD8CNEw")
+
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY
+
+const supabase = createClient(supabaseUrl,supabaseKey)
 
 export default supabase
